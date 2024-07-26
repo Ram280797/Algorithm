fb494d890dcf306cf6b119a0734eb100a7576c7f
Ramkumar <110891811+Ram280797@users.noreply.github.com>
2024-07-26 12:50:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-function findMissingNumber(arr) {
+// function findMissingNumber(arr) {
     
-    const n = arr.length + 1;
+//     const n = arr.length + 1;
 
-   const expectedSum = (n * (n + 1)) / 2;
+//    const expectedSum = (n * (n + 1)) / 2;
 
-    const actualSum = arr.reduce((sum, num) => sum + num, 0);
+//     const actualSum = arr.reduce((sum, num) => sum + num, 0);
 
-    return expectedSum - actualSum;
+//     return expectedSum - actualSum;
+// }
+
+// const inputArray = [1, 2, 3, 4, 6, 7, 8];
+// const missingNumber = findMissingNumber(inputArray);
+// console.log(`Missing Number: ${missingNumber}`);
+
+
+function removeDuplicates(nums) {
+
+    const uniqueNums = Array.from(new Set(nums));
+
+    for (let i = 0; i < uniqueNums.length; i++) {
+        nums[i] = uniqueNums[i];
+    }
+    
+
+    return uniqueNums.length;
 }
 
-const inputArray = [1, 2, 3, 4, 6, 7, 8];
-const missingNumber = findMissingNumber(inputArray);
-console.log(`Missing Number: ${missingNumber}`);
+const nums = [1, 1, 2, 2, 3, 4, 4, 5];
+const newLength = removeDuplicates(nums);
+
+console.log("Array with duplicates removed: " + nums.slice(0, newLength));  2, 3, 4, 5]
